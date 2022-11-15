8e894eca329d10a3a197b7f1eb680e22c52acaf8
Patrick Luiz de Araújo <patrickluizdearaujo@gmail.com>
2022-11-15 17:22:44 -0300

    Minor updates

--- cdf_compiling.py
+++ cdf_compiling.py
@@ -14,7 +14,7 @@ id_numbers = ['607', '8169', '5738']
 
 
 # %%
-csvs_dir = os.path.join('results', 'model_csvs')
+csvs_dir = os.path.join('model_csvs')
 
 for file_name in glob.glob(csvs_dir + '/*.csv'):
     for i in range(len(id_numbers)):
@@ -27,24 +27,30 @@ for tables_csvs, ids in zip(csvs, id_numbers):
     sns.set(font_scale = 2)
     plt.figure(figsize=(20, 14))
 
-    hplt = sns.histplot(tables_csvs.melt(),
-                            x='value',
-                            hue='variable',
-                            element='step',
-                            fill=True,
-                            bins=50,
-                            log_scale=False,
-                            cumulative=True,
-                            alpha=0.7)
+    # hplt = sns.histplot(tables_csvs.melt(),
+    #                         x='value',
+    #                         hue='variable',
+    #                         element='step',
+    #                         fill=True,
+    #                         bins=50,
+    #                         log_scale=False,
+    #                         cumulative=True,
+    #                         alpha=0.7)
+
+    hplt = sns.ecdfplot(data=tables_csvs.melt(),
+                        x='value',
+                        hue='variable',
+                        log_scale=True,
+                        )
 
     plt.setp(hplt.get_legend().get_texts(), fontsize='22') # for legend text
     plt.setp(hplt.get_legend().get_title(), fontsize='32') # for legend title
 
-    hplt.set_ylabel('CDR', fontsize=30)
-    hplt.set_xlabel('Value', fontsize=30)
+    hplt.set_ylabel('Probability', fontsize=30)
+    hplt.set_xlabel('CDR (log)', fontsize=30)
 
     hplt.axes.set_title(f'CDF of tower {ids} value and predictions', fontsize=50)
 
-    plt.savefig(os.path.join('results', 'model_csvs', f'{ids}_results_english_cumulative_nonlog.png'))
+    plt.savefig(os.path.join('model_csvs', f'{ids}_results_english.png'))
     plt.show()
 # %%
